1ff53089df5d28cd5a244d4a76517653e21998c9
ItzSkyReed <prodamsky@gmail.com>
2024-09-03 21:52:23 +0500

    Tweaked `let`

--- table.js
+++ table.js
@@ -127,9 +127,9 @@ class DetailsRenderer {
 
 function hexToRgba(hex, alpha) {
 
-    r = parseInt(hex[1] + hex[2], 16);
-    g = parseInt(hex[3] + hex[4], 16);
-    b = parseInt(hex[5] + hex[6], 16);
+    let r = parseInt(hex[1] + hex[2], 16);
+    let g = parseInt(hex[3] + hex[4], 16);
+    let b = parseInt(hex[5] + hex[6], 16);
 
     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }
